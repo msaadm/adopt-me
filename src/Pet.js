ff1fe90e66d4fb6de06a5c289f22b26ae96d59d7
Muhammad Saad Mateen <saad.5690@gmail.com>
2021-01-12 18:44:30 +0500

    add reach router, add Details route

--- src/Pet.js
+++ src/Pet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 export default function Pet({ name, animal, breed, media, location, id }) {
 
@@ -8,7 +9,7 @@ if(media.length){
 }
 
   return (
-    <a href={`details/${id}`} className="pet">
+    <Link to={`details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -16,6 +17,6 @@ if(media.length){
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   )
 }
