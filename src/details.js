4f201f0bc312094ffe54b3bf9392a097da7dc7b1
Muhammad Saad Mateen <saad.5690@gmail.com>
2021-01-13 18:41:35 +0500

    add modal

--- src/details.js
+++ src/details.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import pet from '@frontendmasters/pet'
-import Carousel from './Carousel'
-import ErrorBoundary from './ErrorBoundary';
-import ThemeContext from './ThemeContext';
+import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
+import { navigate } from "@reach/router";
+import Modal from "./Modal";
 
 // const Details = (props) => {
 //     return (
@@ -12,68 +14,104 @@ import ThemeContext from './ThemeContext';
 //         )
 // }
 
-class Details extends React.Component{
-    // constructor(props){
-    //     super(props);
+class Details extends React.Component {
+  // constructor(props){
+  //     super(props);
 
-    //     this.state = {
-    //         loading: true,
-    //     }
-    // }
+  //     this.state = {
+  //         loading: true,
+  //     }
+  // }
 
-    state = { loading:true };
+  state = { loading: true, showModal: false };
 
-    componentDidMount() {
-        
-        pet.animal(this.props.id).then(({animal}) => {
-            this.setState({
-                name: animal.name,
-                animal: animal.type,
-                location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
-                description: animal.description,
-                media: animal.photos,
-                breed: animal.breeds.primary,
-                loading: false,
-            })
-        }).finally(() => this.setState({loading: false}));
+  componentDidMount() {
+    pet
+      .animal(this.props.id)
+      .then(({ animal }) => {
+        this.setState({
+          url: animal.url,
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false,
+        });
+      })
+      .finally(() => this.setState({ loading: false }));
+  }
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
+  adopt = () => navigate(this.state.url);
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" noThrow />;
+    }
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
     }
 
-    render() {
-        if (this.state.redirect) {
-            return <Redirect to="/" noThrow />;
-          }
-        if(this.state.loading){
-            return <h1>loading...</h1>
-        } 
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      media,
+      showModal,
+    } = this.state;
 
-        const { animal, breed, location, description, name, media } = this.state
-        
-        return (
-            <div className="details">
-                <Carousel media={media} />
-                <div>
-                    <h1>{name}</h1>
-                    <h2>{`${animal} - ${breed} - ${location}`}</h2>
-                    <ThemeContext.Consumer>
-                        {
-                            ([theme]) => (
-                                <button style={{ backgroundColor: theme}} type="button">Adopt {name}</button>
-                            )
-                        }
-                    </ThemeContext.Consumer>
-                    
-                    <p>{description}</p>
-                </div>
-            </div>
-        )
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button
+                style={{ backgroundColor: theme }}
+                type="button"
+                onClick={this.toggleModal}
+              >
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
 
-    }
+          <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <ThemeContext.Consumer>
+                  {([theme]) => (
+                    <button
+                      onClick={this.adopt}
+                      style={{ backgroundColor: theme }}
+                    >
+                      Yes
+                    </button>
+                  )}
+                </ThemeContext.Consumer>
+                <button onClick={this.toggleModal}>No, I'm a monster</button>
+              </div>
+            </Modal>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default function DetailsWithErrorBoundary(props) {
-    return (
-        <ErrorBoundary>
-            <Details {...props}/>
-        </ErrorBoundary>
-    )
-};
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
